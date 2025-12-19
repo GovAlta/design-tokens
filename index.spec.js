de6ff75d02cb7decec0725a221719451463b3a98
Benji Franck <benjamin.franck@gmail.com>
2025-12-19 09:17:55 -0700

    spike(#3285): add experimental tokens

--- index.spec.js
+++ index.spec.js
@@ -11,16 +11,26 @@ describe("GoA Design Tokens", () => {
   it("should create css and scss files", async () => {
     SC.generate("./tmp");
 
-    const cssfiles = fs.readdirSync("./tmp/dist");
-    expect(cssfiles.length).toBe(2);
-    expect(cssfiles[0]).toBe(`tokens.css`);
-    expect(cssfiles[1]).toBe(`tokens.scss`);
+    const cssfiles = fs.readdirSync("./tmp/dist").sort();
+    expect(cssfiles.length).toBe(3);
+    expect(cssfiles).toEqual([
+      "experimental-tokens.css",
+      "tokens.css",
+      "tokens.scss",
+    ]);
   });
 
   it("should create valid css output", async () => {
     SC.generate("./tmp");
     const raw = fs.readFileSync("./tmp/dist/tokens.css", { encoding: "utf8" });
     expect(raw).not.toContain("[object Object]");
+  });
+
+  it("should create valid experimental css output", async () => {
+    SC.generate("./tmp");
+    const raw = fs.readFileSync("./tmp/dist/experimental-tokens.css", { encoding: "utf8" });
+    expect(raw).not.toContain("[object Object]");
+    expect(raw).toContain(".v2-experimental-tokens");
   });
   it("should create valid scss output", async () => {
     SC.generate("./tmp");
